0a48ab3eb07292f7839cb781b23e5080181bf3aa
Invader Zim <pwstern@gmail.com>
2019-05-19 12:05:35 -0700

    Initial setup of files.

--- NumberSlide/NumberSlideVC.swift
+++ NumberSlide/NumberSlideVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NumberSlideVC.swift
 //  NumberSlide
 //
 //  Created by Phil Stern on 5/19/19.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NumberSlideVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
